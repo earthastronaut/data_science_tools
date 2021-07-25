471dea30f1407f638b04023c693307c3cf00884c
Dylan <an.email0101@gmail.com>
2021-07-24 21:37:22 -0600

    make importing all modules optional so import is fast when you only want particular tools

--- data_science_tools/__init__.py
+++ data_science_tools/__init__.py
@@ -1,23 +1,52 @@
 """ Data Science Tools
 """
 # flake8: noqa: F403,F401
-from . import config
+# pylint: disable=import-outside-toplevel,global-statement
 
 
 def get_version():
     """Get version number"""
-    # TODO: remove the redefine-outer-name when removing *
-    import os  # pylint: disable=redefined-outer-name,import-outside-toplevel
+    import os  # pylint: disable=redefined-outer-name
 
     with open(os.path.join(os.path.dirname(__file__), "__version__")) as buffer:
         return buffer.readline().strip()
 
 
 __version__ = get_version()
-__all__ = ["config", "__version__"]
+__all__ = ["__version__"]
 
-# --------------------------------------------------------------------------- #
-# Configuration
-# --------------------------------------------------------------------------- #
 
-config.initialize()
+def import_all_modules():
+    """Load all modules"""
+    global __all__
+
+    from . import (
+        dataframe,
+        graph,
+        python_interactive,
+        quantize,
+        statistics,
+        utils,
+        weighted,
+        matplotlib_tools,
+        plotly_tools,
+    )
+
+    globals().update(locals())
+
+    modules = [
+        dataframe,
+        graph,
+        python_interactive,
+        quantize,
+        statistics,
+        utils,
+        weighted,
+        matplotlib_tools,
+        plotly_tools,
+    ]
+
+    for mod in modules:
+        all_public = getattr(mod, "__all__", [])
+        __all__ += all_public
+        globals().update({name: getattr(mod, name) for name in all_public})
