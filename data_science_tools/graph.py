aaf373c54f14b364f2b35a7a46a1df2987e41261
Dylan <an.email0101@gmail.com>
2021-07-24 18:11:06 -0600

    add searching graph

--- data_science_tools/graph.py
+++ data_science_tools/graph.py
@@ -3,21 +3,56 @@
 from collections import deque
 import logging
 
-from .types import (
+from typing import (
     Any,
     Callable,
     Deque,
+    Dict,
     Sequence,
-    Node,
+    Hashable,
     Union,
+    TypeVar,
 )
 
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
+Node = TypeVar("Node", Hashable, str, int, float)
+
 
 class SearchGraphCycleError(Exception):
     """Raise this error when a cycle is found when searching a graph"""
+
+
+def cycle_check_child_in_path_n(
+    count: int,
+) -> Callable[[Node, Sequence[Node], Dict[Node, int]], bool]:
+    """Create a function which checks if the child has been in the path n times.
+
+    Parameters
+        count: number of times a child can appear in the path.
+
+    Returns
+        bool: if >= count
+    """
+
+    def cycle_check(
+        child: Node,
+        path: Sequence[Node],  # pylint: disable=unused-argument
+        path_lookup: Dict[Node, int],
+    ) -> bool:
+        return path_lookup.get(child, -1) >= count
+
+    return cycle_check
+
+
+def cycle_check_child_in_path(
+    child: Node,
+    path: Sequence[Node],  # pylint: disable=unused-argument
+    path_lookup: Dict[Node, int],
+) -> bool:
+    """Check if child node is in the path"""
+    return child in path_lookup
 
 
 def search_graph(
@@ -25,10 +60,14 @@ def search_graph(
     get_children: Callable[[Node], Sequence[Node]],
     callback: Callable[[Sequence[Node]], Union[bool, None]] = None,
     callback_no_children: Callable[[Sequence[Node]], Any] = None,
+    callback_cycle_check: Callable[
+        [Node, Sequence[Node], Dict[Node, int]], bool
+    ] = cycle_check_child_in_path,
     depth_first: bool = True,
     raise_cycle_error: bool = True,
 ):
     """Graph search function avoiding cicular patterns.
+
     Parameters
         start: Node (aka key) in the graph (aka dict)
         get_children: Returns children nodes in the graph. If graph is
@@ -40,6 +79,11 @@ def search_graph(
         callback_no_children: If provided, calls `callback_no_children(path)` when
             finds an end node with no children. Note: if your get_children is a
             generator this will not work.
+        callback_cycle_check: Will call with the child and the path and decide
+            if there is a cycle in the graph. Provides a hash lookup of the number
+            of times a child has been visited. By default if child is in this
+            lookup then it returns true and raises error or stops depending on the
+            raise_cycle_error.
         depth_first: If True then does a depth first search
             otherwise it's dreath first
         raise_cycle_error: If True then raise an error if cycle is found otherwise
@@ -50,7 +94,7 @@ def search_graph(
             `callable(path) == True`.
     """
     stack: Deque = deque()
-    stack.append((start, [start], {start}))
+    stack.append((start, [start], {start: 1}))
     pop_node = stack.pop if depth_first else stack.popleft
 
     while len(stack) > 0:
@@ -68,8 +112,12 @@ def search_graph(
             continue
 
         for child in children:
-            if child in path_lookup:
+            if callback_cycle_check(child, path, path_lookup):
                 if raise_cycle_error:
                     raise SearchGraphCycleError(path + [child])
                 continue
-            stack.append((child, path + [child], path_lookup.union({child})))
+
+            child_path_lookup = path_lookup.copy()
+            child_path_lookup.setdefault(child, 0)
+            child_path_lookup[child] += 1
+            stack.append((child, path + [child], child_path_lookup))
