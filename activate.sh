c424c856c519498da071385939a63dfbf9b833d0
Dylan <an.email0101@gmail.com>
2021-07-24 19:07:25 -0600

    add python env activate script

---
+++ activate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PROJECT_PATH=$(git rev-parse --show-toplevel)
+VENV=${PROJECT_PATH}/.venv
+
+# check for python3
+if ! command -v python3 &> /dev/null
+then
+    >&2 echo "ERROR: could  not find python3"
+    exit 1
+fi
+
+# activate virtual environment
+if [ -d "${VENV}" ] 
+then
+    echo "Found virtual environment ${VENV}"
+    source ${VENV}/bin/activate
+else
+    echo "Create python virtual environment ${VENV}"
+    python3 -m venv ${VENV}
+    echo export PATH=${PROJECT_PATH}/scripts/bin:\$PATH >> ${VENV}/bin/activate
+    source ${VENV}/bin/activate
+    pip3 install --upgrade pip setuptools wheel
+    pip3 install -r ${PROJECT_PATH}/requirements.txt
+fi
